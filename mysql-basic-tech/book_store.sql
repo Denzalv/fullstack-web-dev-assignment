de2ea96a4676162ffadf8e198edad946f605cb18
Deni Alpiana <Denzalv01@gmail.com>
2021-11-28 10:49:31 +0700

    update baru sampai create table (mati lampu)

--- mysql-basic-tech/book_store.sql
+++ mysql-basic-tech/book_store.sql
@@ -2,7 +2,27 @@
 SHOW DATABASES;
 
 -- Create Database --
-CREATE DATABASE student;
+CREATE DATABASE bookstore;
+
+-- Use Database --
+USE bookstore;
+
+-- Show Table --
+SHOW tables;
+
+-- Create table --
+CREATE table books (
+	id INT AUTO_INCREMENT,
+	author1 VARCHAR(100) NOT NULL,
+	author2 VARCHAR(100),
+	author3 VARCHAR(100),
+	title VARCHAR(100),
+	description TEXT,
+	place_sell VARCHAR(3),
+	stock INT DEFAULT 0,
+	creation_date DATETIME DEFAULT CURRENT_TIMESTAMP(),
+	PRIMARY KEY (id)
+);
 
 -- Insert Data --
-INSERT INTO student (id, name, age) VALUES(1, 'Terra', 2);
+INSERT INTO bookstore (id, name, age) VALUES(1, 'Terra', 2);
